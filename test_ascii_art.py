58785d1028e1305f3ee12130cbd992e382623bde
Romain ODET <romain.odet@lecole-ldlc.com>
2018-10-07 19:15:01 +0200

    Adding the file with the ascii art.

---
+++ test_ascii_art.py
@@ -0,0 +1,22 @@
+"""
+                 /
+    |      /  |////:  o////  ./////    s`      y////-
+    |         |       /      s-    s.  h`      h
+    |         |////.  /      s-    s-  h`      d////`
+    |         |       /      s-    s.  h`      h
+    |////     |////:  o/::/  -o/::/o   y////.  d////-
+
+        -mmmmm.   ymmmmmmmmmy-   ymmmm-       -ymmmmmmmo
+        -mmmmm.   ymmmm+ohmmmm-  ymmmm-     :mmmmdo/+s+
+        -mmmmm.   ymmm     hmmmo ymmmm-    hmmmm:
+        -mmmmm:   hmmm     mmmm/ ymmmm/    ymmmmy:
+        -mmmmmmmmmmmmmmmmmmmms   ymmmmmmmm  dommmmmmmmo
+        -hhhhhhhhhhhhhhhhys+/    shhhhhhhhy  ./shddhh/
+        
+        
+     file :test_ascii_art.py
+     By: romain.odet <romain.odet@lecole-ldlc.com>           
+     Created: 07/10/2018 19:02 by Romain ODET  
+"""
+
+print("")
